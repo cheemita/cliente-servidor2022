33a7ff62f41390a8459189ffaff214da6d4da251
Josema <gjosemanuel375@gmail.com>
2022-05-23 15:35:32 -0500

    css

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/css', express.static(__dirname + /node_modules/bootstrap/dist/css));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
